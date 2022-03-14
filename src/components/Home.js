e2ad2ef125d08ced36ca1fad3f3cea25e9d01a02
nedu'm <davidsonchinedum@gmail.com>
2022-03-14 17:42:08 +0100

    made navbar responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
 const Home = () => {
   return (
     <Wrapper>
-
+    <h1>Home</h1>
     </Wrapper>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,54 +1,53 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
-
+import { FiMenu, FiX } from 'react-icons/fi';
 import '../styles/Navbar.css'
 
-const Nav = styled.div``;
-
-export default function NavBar() {
+const Navbar = () => {
+  const Nav = styled.div``;
   const [open, setOpen] = useState(false);
 
-  const handleClick = () => { 
-    setOpen(!open);
-  };
+	const handleClick = () => {
+		setOpen(!open);
+	};
 
-  const closeMenu = () => { 
-    setOpen(false);
-  };
+	const closeMenu = () => {
+		setOpen(false);
+	};
 
-  return (
-    <Nav className='navbar'>
-      <Link to='/' className='nav-logo' onClick={() => setOpen(false)}>
-        Logo
-      </Link>
-      <ul className={open ? 'nav-link active' : 'nav-links'}>
-        <li className='nav-item'>
-          <Link to="/" className='nav-link' onClick={() => setOpen(false)}>
-            Home
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link to="/about" className='nav-link' onClick={() => setOpen(false)}>
-            About
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link to="/explore" className='nav-link' onClick={() => setOpen(false)}>
-            Explore
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link to="/waitlist" className='nav-link' onClick={() => setOpen(false)}>
-            WaitList
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link to="/contact" className='nav-link' onClick={() => setOpen(false)}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </Nav>
-  )
+	return (
+		<Nav className="navbar">
+			<Link to="/" className="nav-logo">
+				Logo
+			</Link>
+			<div onClick={handleClick} className="nav-icon">
+				{open ? <FiX /> : <FiMenu />}
+			</div>
+			<ul className={open ? 'nav-links active' : 'nav-links'}>
+				<li className="nav-item">
+					<Link to="/" className="nav-link" onClick={closeMenu}>
+						Home
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/about" className="nav-link" onClick={closeMenu}>
+						About
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/shop" className="nav-link" onClick={closeMenu}>
+						Shop
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/contact" className="nav-link" onClick={closeMenu}>
+						Contact
+					</Link>
+				</li>
+			</ul>
+		</Nav>
+	);
 };
+
+export default Navbar;
