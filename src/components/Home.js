a3a0d07a0491c747b6f05a53ad9be9b08f9adf17
nedu'm <davidsonchinedum@gmail.com>
2022-04-01 18:41:07 +0100

    image and logo responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components'
+import '../styles/homepage.css'
 
-const Wrapper = styled.div`
-  font-size: '2rem';
-  display: 'flex';
-  justify-content: center;
-  margin-top: '2rem';
+const ImageWrapper = styled.div`
+  margin-top: 1.6rem;
+  margin-left: 8.33333vw;
+  margin-right: 8.33333vw;
+  margin-bottom: 8.33333vw;
+  position: relative;
+  min-height: calc(100vh - 120px);
 `;
 
 const Home = () => {
   return (
-    <Wrapper>
-    <h1>Home</h1>
-    </Wrapper>
+    <ImageWrapper>
+      <div className='bg-image'>
+        <svg className='bg-name' viewBox='0 0 430 104'>
+          <h1>Ableton</h1>
+          <g fill='#FD5948'>
+          </g>
+        </svg>
+      </div>
+    </ImageWrapper>
   );
 };
-
+  
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,12 @@ const Navbar = () => {
 	return (
 		<Nav className="navbar">
 			<Link to="/" className="nav-logo">
-				Logo
+				<a class="nav__logo" href="/en/">
+					<svg role="img" aria-labelledby="logo" class="nav__logo__image" xmlns="http://www.w3.org/2000/svg" width="45" height="21" viewBox="0 0 45 21">
+						<title id="logo">Ableton Homepage</title>
+						<path d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z"></path>
+					</svg>
+				</a>
 			</Link>
 			<div onClick={handleClick} className="nav-icon">
 				{open ? <FiX /> : <FiMenu />}
